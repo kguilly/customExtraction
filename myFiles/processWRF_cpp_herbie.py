75b768d0e639418c40b83aea338865a53fd3eba2
kguilly <115897953+kguilly@users.noreply.github.com>
2023-02-07 12:21:38 -0600

    need to debug recent changes

--- myFiles/processWRF_cpp_herbie.py
+++ myFiles/processWRF_cpp_herbie.py
@@ -499,7 +499,7 @@ class formatWRF():
             hourly_file_path += filepathsep[i] + '/'
         hourly_file_path += "Daily_Monthly/"
 
-        df_states = pd.DataFrame()
+        df_states = {}
         for fips_folder in sorted(os.listdir(hourly_file_path)):
             fips_dir = hourly_file_path + fips_folder + '/'
             for monthly_file in sorted(os.listdir(fips_dir)):
@@ -508,7 +508,36 @@ class formatWRF():
                 year = monthly_file[14:18]
                 month = monthly_file[18:20]
                 col_name = state_fips + '_' + year + '_' + month
-                df_states[col_name]
+                full_path = fips_dir+monthly_file
+                if not col_name in df_states:
+                    df_states[col_name] = []
+                df_states[col_name].append(full_path)
+
+        # the dictionary is now built, now we need to merge them into stately files
+        for col in df_states:
+            df_out = pd.DataFrame()
+            for file in df_states[col]:
+                df = pd.read_csv(file)
+                df_out = df_out.merge(df, how='outer')
+
+            file_path_out = ''
+            file_path_out_sep = self.wrf_data_path.split('/')
+            for i in range(len(file_path_out_sep) - 1):
+                if file_path_out_sep[i].rfind("Hourly") != -1:
+                    break
+                file_path_out += file_path_out_sep[i] + '/'
+
+            in_file_path_sep = df_states[col][0].split('/')
+            needed_info_sep = in_file_path_sep[-1].split('_')
+            fips = needed_info_sep[1]
+            yyyymmcsv = needed_info_sep[3]
+            state_fips = fips[0:2]
+            year = yyyymmcsv[0:4]
+            month = yyyymmcsv[4:6]
+            state_abbrev = needed_info_sep[2]
+            file_path_out += 'data/' + year + '/' + state_abbrev + '/' + 'HRRR_' + state_fips + '_' + \
+                             state_abbrev + '_' + year + '-' + month + '.csv'
+            df_out.to_csv(file_path_out)
 
 f = formatWRF()
 f.main()
