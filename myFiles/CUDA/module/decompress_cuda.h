b3fdeb0afb851ff3d4968fae5dfb4326488b6021
kguilly <115897953+kguilly@users.noreply.github.com>
2023-03-15 12:56:42 -0500

    experimenting with cuda

---
+++ myFiles/CUDA/module/decompress_cuda.h
@@ -0,0 +1,12 @@
+#ifndef DECOMPRESS_CUDA_H
+#define DECOMPRESS_CUDA_H
+
+#ifdef ___cplusplus
+extern "C" {
+    #endif
+    void decompress_with_cuda(/*inputs*/);
+    #ifdef __cplusplus
+}
+#endif
+#endif /* DECOMPRESS_CUDA_H */
+
--- myFiles/Tests/cuda_test_0.cu
+++ myFiles/Tests/cuda_test_0.cu
@@ -2,20 +2,19 @@
 // I also want to test how to get information about the GPU
 #include <iostream>
 #include <math.h>
+#include <stdlib.h>
+#include <ctime>
 // Kernel function to add the elements of two arrays
 
-__global__
-void add(int, float*, float*);
+__global__ void AddInts(int*, int*, int);
 
 int main(void)
 {
 
-  int N = 1<<20; // I think thats a binary 1 shifted left 20 times
-  float *x, *y;
+  srand(time(NULL));
 
   /////////////////////////////////////////////////////////////////////
   // Get device properties
-
   int num_devices;
   cudaGetDeviceCount(&num_devices);
   for(int i=0; i < num_devices; i++){
@@ -34,45 +33,78 @@ int main(void)
     std::cout << "Total Memory: " << total_mem << "\t Free Memory: " << free_mem << std::endl;
 
   }
-  // Allocate Unified Memory – accessible from CPU or GPU
-  cudaMallocManaged(&x, N*sizeof(float));
-  cudaMallocManaged(&y, N*sizeof(float));
 
-  // initialize x and y arrays on the host
-  for (int i = 0; i < N; i++) {
-    x[i] = 1.0f;
-    y[i] = 2.0f;
+  int N = 1<<20; // I think thats a binary 1 shifted left 20 times
+  int* h_x = new int[N];
+  int* h_y = new int[N];
+  //make host version of the arrays
+  for (int i=0; i<N; i++) {
+    h_x[i] = rand() % 1000;
+    h_y[i] = rand() % 1000;
   }
 
-  // make a nice block size
-  int blockSize = 256;
-  int numBlocks = (N + blockSize - 1) / blockSize;
+  for (int i=0; i<5; i++) {
+    std::cout << h_x[i] << " " << h_y[i] << std::endl;
+  }
 
-  // Run kernel on 1M elements on the GPU
-  add<<<numBlocks, blockSize>>>(N, x, y);
+  // Make device versions of the host variables
+  int *d_x, *d_y;
+  if (cudaMalloc(&d_x, sizeof(int)*N) != cudaSuccess){
+    std::cout << "d_x could not be alloced" << std::endl;
+    return 0;
+  }
 
-  // Wait for GPU to finish before accessing on host
-  cudaDeviceSynchronize();
+  if (cudaMalloc(&d_y, sizeof(int)*N) != cudaSuccess){
+    std::cout << "d_y could not be alloced" << std::endl;
+    cudaFree(d_x);
+    return 0;
+  }
 
-  // Check for errors (all values should be 3.0f)
-  float maxError = 0.0f;
-  for (int i = 0; i < N; i++)
-    maxError = fmax(maxError, fabs(y[i]-3.0f));
-  std::cout << "Max error: " << maxError << std::endl;
+  // Now copy the values over to the GPU
+  if (cudaMemcpy(d_x, h_x, sizeof(int) * N, cudaMemcpyHostToDevice) != cudaSuccess){
+    std::cout << "Could not copy d_x" << std::endl;
+    cudaFree(d_x);
+    cudaFree(d_y);
+    return 0;
+  }
 
-  // Free memory
-  cudaFree(x);
-  cudaFree(y);
+  if (cudaMemcpy(d_y, h_y, sizeof(int) * N, cudaMemcpyHostToDevice) != cudaSuccess){
+    std::cout << "Could not copy d_y" << std::endl;
+    cudaFree(d_x);
+    cudaFree(d_y);
+    return 0;
+  }
+
+  AddInts<<<N / 256 + 1, 256 >>>(d_x, d_y, N);
+
+  if (cudaMemcpy(h_x, d_x, sizeof(int) * N, cudaMemcpyDeviceToHost) != cudaSuccess) {
+    delete[] h_x;
+    delete[] h_y;  
+    cudaFree(d_x);
+    cudaFree(d_y);
+    std::cout << "The stuff could not be copied back to the host" << std::endl;
+    return 0;
+  }
+
+  // check to see that it did everything correctly
+  std::cout << "\n After Addition: " << std::endl;
+  for (int i=0; i<5; i++) {
+    std::cout << h_x[i] << std::endl;
+  }
+
+  cudaFree(d_x);
+  cudaFree(d_y);
+  delete[] h_x;
+  delete[] h_y;
   
   return 0;
 }
 
-__global__
-void add(int n, float *x, float *y){
+__global__ void AddInts(int* a, int* b, int count){
   
-  int index = blockIdx.x * blockDim.x + threadIdx.x;
-  int stride = blockDim.x * gridDim.x;
-  for (int i = index; i < n; i += stride)
-    y[i] = x[i] + y[i];
+  int id = blockIdx.x * blockDim.x + threadIdx.x;
+  if (id < count) {
+    a[id] += b[id];
+  }
   
 }
