dac9a1306b12321d795c0ef0fee436f8379b8a62
kguilly <115897953+kguilly@users.noreply.github.com>
2023-04-15 17:33:32 -0500

    working on the test module

--- myFiles/CUDA/test_module2/decompress_grib_test.h
+++ myFiles/CUDA/test_module2/decompress_grib_test.h
@@ -2,8 +2,9 @@
 #define DECOMPRESS_GRIB_TEST_H
 
 #include <cuda_runtime.h>
+#include "shared_test_objs.h"
 
-station_t* extract_indexes(station_t *);
+station_t* extract_indexes(station_t *, double*, double*, int, int);
 __global__ void cuda_find_nearest(station_t*, double*, double*, int, int);
 
 
